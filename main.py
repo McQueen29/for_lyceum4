028978d00d6c097233bffdcb8df944351c3520c9
USER <domin7135@gmail.com>
2021-11-23 20:39:57 +0300

    first commit

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import sys
 from random import randint
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setGeometry(300, 300, 500, 500)
+        self.setWindowTitle('окружности')
+        self.btn_create = QPushButton('Создать', self)
+        self.btn_create.resize(100, 50)
+        self.btn_create.move(0, 0)
         self.do_paint = False
         self.btn_create.clicked.connect(self.paint)
 
@@ -24,11 +27,14 @@ class Example(QWidget):
         self.repaint()
 
     def draw_circle(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        r = randint(0, 255)
+        g = randint(0, 255)
+        b = randint(0, 255)
+        qp.setBrush(QColor(r, g, b))
         rx = randint(75, 450)
         yx = randint(75, 450)
-        r = randint(1, 50)
-        qp.drawEllipse(rx, yx, r, r)
+        R = randint(1, 50)
+        qp.drawEllipse(rx, yx, R, R)
 
 
 if __name__ == '__main__':
